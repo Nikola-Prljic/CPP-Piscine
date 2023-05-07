c2640ea209fb2f1c204f64dcf4b25a0795efacbd
Niki42 <nik.7377@gmail.com>
2023-05-07 16:24:54 +0200

    add second try lol

--- Module 05/ex00/main.cpp
+++ Module 05/ex00/main.cpp
@@ -4,11 +4,16 @@ int main()
 {
     try 
     {
-        Bureaucrat nik("nik", 151);
+        Bureaucrat nik("nik", -42);
         std::cout << nik << std::endl;
         nik.increment();
-        //nik.decrement();
-    } catch (Bureaucrat::ToHigh &e) 
+        nik.decrement();
+    } 
+    catch (Bureaucrat::ToHigh &e) 
+    {
+        std::cout << e.what() << std::endl;
+    }
+    catch (Bureaucrat::ToLow &e) 
     {
         std::cout << e.what() << std::endl;
     }
