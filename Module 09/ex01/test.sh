4c7b506e6c7ca62e4da55317eec88f13032b65db
Niki42 <nik.7377@gmail.com>
2023-07-20 20:35:05 +0200

    valgrind tests fixed and did some tests for the tests

--- Module 09/ex01/test.sh
+++ Module 09/ex01/test.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+num=0
+
 make
 ./RPN "5 1 -" > out 2>&1
 valgrind ./RPN "5 1 -" > valgrind_test.txt 2>&1
@@ -6,7 +8,6 @@ grep -q "ERROR SUMMARY: 0 errors" valgrind_test.txt
 val_error=$?
 grep -q "All heap blocks were freed -- no leaks are possible" valgrind_test.txt
 val_blocks=$?
-num=-1
 num=$(($num + $val_error + $val_blocks))
 
 ./RPN "1 2 -" >> out 2>&1
@@ -170,13 +171,12 @@ val_blocks=$?
 num=$(($num + $val_error + $val_blocks))
 
 ./RPN "1 2 *" a >> out 2>&1
-valgrind ./RPN "1 2 *" > valgrind_test.txt 2>&1
+valgrind ./RPN "1 2 *" a> valgrind_test.txt 2>&1
 grep -q "ERROR SUMMARY: 0 errors" valgrind_test.txt
 val_error=$?
 grep -q "All heap blocks were freed -- no leaks are possible" valgrind_test.txt
 val_blocks=$?
 num=$(($num + $val_error + $val_blocks))
-
 
 if [ $num -eq 0 ]
     then echo valgrind: ok
@@ -193,7 +193,7 @@ fi
 diff out in
 
 if [ $? -eq 0 ]
-    then echo ok
+    then echo "tests:    ok"
     rm out
 else
     echo ko
