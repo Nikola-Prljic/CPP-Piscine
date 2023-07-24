9d68768a938855e05245a5f30256c693b82662be
Niki <nik.7377@gmail.com>
2023-07-24 13:32:02 +0200

    fixed tester

--- Module 09/ex01/test.sh
+++ Module 09/ex01/test.sh
@@ -1,182 +1,40 @@
 #!/bin/sh
 num=0
-
 make
-./RPN "5 1 -" > out 2>&1
-valgrind ./RPN "5 1 -" > valgrind_test.txt 2>&1
-grep -q "ERROR SUMMARY: 0 errors" valgrind_test.txt
-val_error=$?
-grep -q "All heap blocks were freed -- no leaks are possible" valgrind_test.txt
-val_blocks=$?
-num=$(($num + $val_error + $val_blocks))
 
-./RPN "1 2 -" >> out 2>&1
-valgrind ./RPN "1 2 -" > valgrind_test.txt 2>&1
-grep -q "ERROR SUMMARY: 0 errors" valgrind_test.txt
-val_error=$?
-grep -q "All heap blocks were freed -- no leaks are possible" valgrind_test.txt
-val_blocks=$?
-num=$(($num + $val_error + $val_blocks))
+ft_test()
+{
+    ./RPN "$1" $2 $3 >> out 2>&1
+    valgrind ./RPN $1 > valgrind_test.txt 2>&1
+    grep -q "ERROR SUMMARY: 0 errors" valgrind_test.txt
+    val_error=$?
+    grep -q "All heap blocks were freed -- no leaks are possible" valgrind_test.txt
+    val_blocks=$?
+    num=$(($num + $val_error + $val_blocks))
+}
 
-./RPN "2 1 -" >> out 2>&1
-valgrind ./RPN "2 1 -" > valgrind_test.txt 2>&1
-grep -q "ERROR SUMMARY: 0 errors" valgrind_test.txt
-val_error=$?
-grep -q "All heap blocks were freed -- no leaks are possible" valgrind_test.txt
-val_blocks=$?
-num=$(($num + $val_error + $val_blocks))
-
-./RPN "1 2 * 2 / 2 * 2 4 - +" >> out 2>&1
-valgrind ./RPN "1 2 * 2 / 2 * 2 4 - +" > valgrind_test.txt 2>&1
-grep -q "ERROR SUMMARY: 0 errors" valgrind_test.txt
-val_error=$?
-grep -q "All heap blocks were freed -- no leaks are possible" valgrind_test.txt
-val_blocks=$?
-num=$(($num + $val_error + $val_blocks))
-
-./RPN "8 9 * 9 - 9 - 9 - 4 - 1 +" >> out 2>&1
-valgrind ./RPN "8 9 * 9 - 9 - 9 - 4 - 1 +" > valgrind_test.txt 2>&1
-grep -q "ERROR SUMMARY: 0 errors" valgrind_test.txt
-val_error=$?
-grep -q "All heap blocks were freed -- no leaks are possible" valgrind_test.txt
-val_blocks=$?
-num=$(($num + $val_error + $val_blocks))
-
-./RPN "" >> out 2>&1
-valgrind ./RPN "8 9 * 9 - 9 - 9 - 4 - 1 +" > valgrind_test.txt 2>&1
-grep -q "ERROR SUMMARY: 0 errors" valgrind_test.txt
-val_error=$?
-grep -q "All heap blocks were freed -- no leaks are possible" valgrind_test.txt
-val_blocks=$?
-num=$(($num + $val_error + $val_blocks))
-
-./RPN "a" >> out 2>&1
-valgrind ./RPN "" > valgrind_test.txt 2>&1
-grep -q "ERROR SUMMARY: 0 errors" valgrind_test.txt
-val_error=$?
-grep -q "All heap blocks were freed -- no leaks are possible" valgrind_test.txt
-val_blocks=$?
-num=$(($num + $val_error + $val_blocks))
-
-./RPN "7 2 *c" >> out 2>&1
-valgrind ./RPN "7 2 *c" > valgrind_test.txt 2>&1
-grep -q "ERROR SUMMARY: 0 errors" valgrind_test.txt
-val_error=$?
-grep -q "All heap blocks were freed -- no leaks are possible" valgrind_test.txt
-val_blocks=$?
-num=$(($num + $val_error + $val_blocks))
-
-./RPN "*7 2 *c" >> out 2>&1
-valgrind ./RPN "*7 2 *c" > valgrind_test.txt 2>&1
-grep -q "ERROR SUMMARY: 0 errors" valgrind_test.txt
-val_error=$?
-grep -q "All heap blocks were freed -- no leaks are possible" valgrind_test.txt
-val_blocks=$?
-num=$(($num + $val_error + $val_blocks))
-
-./RPN "7 2 * c" >> out 2>&1
-valgrind ./RPN "7 2 * c" > valgrind_test.txt 2>&1
-grep -q "ERROR SUMMARY: 0 errors" valgrind_test.txt
-val_error=$?
-grep -q "All heap blocks were freed -- no leaks are possible" valgrind_test.txt
-val_blocks=$?
-num=$(($num + $val_error + $val_blocks))
-
-./RPN "7c2 *" >> out 2>&1
-valgrind ./RPN "7c2 *" > valgrind_test.txt 2>&1
-grep -q "ERROR SUMMARY: 0 errors" valgrind_test.txt
-val_error=$?
-grep -q "All heap blocks were freed -- no leaks are possible" valgrind_test.txt
-val_blocks=$?
-num=$(($num + $val_error + $val_blocks))
-
-./RPN "7c2" >> out 2>&1
-valgrind ./RPN "7c2" > valgrind_test.txt 2>&1
-grep -q "ERROR SUMMARY: 0 errors" valgrind_test.txt
-val_error=$?
-grep -q "All heap blocks were freed -- no leaks are possible" valgrind_test.txt
-val_blocks=$?
-num=$(($num + $val_error + $val_blocks))
-
-./RPN "      " >> out 2>&1
-valgrind ./RPN "      " > valgrind_test.txt 2>&1
-grep -q "ERROR SUMMARY: 0 errors" valgrind_test.txt
-val_error=$?
-grep -q "All heap blocks were freed -- no leaks are possible" valgrind_test.txt
-val_blocks=$?
-num=$(($num + $val_error + $val_blocks))
-
-./RPN "  2 *" >> out 2>&1
-valgrind ./RPN "  2 *" > valgrind_test.txt 2>&1
-grep -q "ERROR SUMMARY: 0 errors" valgrind_test.txt
-val_error=$?
-grep -q "All heap blocks were freed -- no leaks are possible" valgrind_test.txt
-val_blocks=$?
-num=$(($num + $val_error + $val_blocks))
-
-./RPN "******" >> out 2>&1
-valgrind ./RPN "******" > valgrind_test.txt 2>&1
-grep -q "ERROR SUMMARY: 0 errors" valgrind_test.txt
-val_error=$?
-grep -q "All heap blocks were freed -- no leaks are possible" valgrind_test.txt
-val_blocks=$?
-num=$(($num + $val_error + $val_blocks))
-
-./RPN "2 2 #" >> out 2>&1
-valgrind ./RPN "2 2 #" > valgrind_test.txt 2>&1
-grep -q "ERROR SUMMARY: 0 errors" valgrind_test.txt
-val_error=$?
-grep -q "All heap blocks were freed -- no leaks are possible" valgrind_test.txt
-val_blocks=$?
-num=$(($num + $val_error + $val_blocks))
-
-./RPN "2 2.*" >> out 2>&1
-valgrind ./RPN "2 2.*" > valgrind_test.txt 2>&1
-grep -q "ERROR SUMMARY: 0 errors" valgrind_test.txt
-val_error=$?
-grep -q "All heap blocks were freed -- no leaks are possible" valgrind_test.txt
-val_blocks=$?
-num=$(($num + $val_error + $val_blocks))
-
-./RPN "2 2 * * *" >> out 2>&1
-valgrind ./RPN "2 2 * * *" > valgrind_test.txt 2>&1
-grep -q "ERROR SUMMARY: 0 errors" valgrind_test.txt
-val_error=$?
-grep -q "All heap blocks were freed -- no leaks are possible" valgrind_test.txt
-val_blocks=$?
-num=$(($num + $val_error + $val_blocks))
-
-./RPN "2 2 2 * *" >> out 2>&1
-valgrind ./RPN "2 2 2 * *" > valgrind_test.txt 2>&1
-grep -q "ERROR SUMMARY: 0 errors" valgrind_test.txt
-val_error=$?
-grep -q "All heap blocks were freed -- no leaks are possible" valgrind_test.txt
-val_blocks=$?
-num=$(($num + $val_error + $val_blocks))
-
-./RPN "2 2 2 2 2 2 + + + + +ä" >> out 2>&1
-valgrind ./RPN "2 2 2 2 2 2 + + + + +ä" > valgrind_test.txt 2>&1
-grep -q "ERROR SUMMARY: 0 errors" valgrind_test.txt
-val_error=$?
-grep -q "All heap blocks were freed -- no leaks are possible" valgrind_test.txt
-val_blocks=$?
-num=$(($num + $val_error + $val_blocks))
-
-./RPN >> out 2>&1
-valgrind ./RPN > valgrind_test.txt 2>&1
-grep -q "ERROR SUMMARY: 0 errors" valgrind_test.txt
-val_error=$?
-grep -q "All heap blocks were freed -- no leaks are possible" valgrind_test.txt
-val_blocks=$?
-num=$(($num + $val_error + $val_blocks))
-
-./RPN "1 2 *" a >> out 2>&1
-valgrind ./RPN "1 2 *" a> valgrind_test.txt 2>&1
-grep -q "ERROR SUMMARY: 0 errors" valgrind_test.txt
-val_error=$?
-grep -q "All heap blocks were freed -- no leaks are possible" valgrind_test.txt
-val_blocks=$?
-num=$(($num + $val_error + $val_blocks))
+ft_test "5 1 -"
+ft_test "1 2 -"
+ft_test "2 1 -"
+ft_test "1 2 * 2 / 2 * 2 4 - +"
+ft_test "8 9 * 9 - 9 - 9 - 4 - 1 +"
+ft_test ""
+ft_test "a"
+ft_test "7 2 *c"
+ft_test "*7 2 *c"
+ft_test "7 2 * c"
+ft_test "7c2 *"
+ft_test "7c2"
+ft_test "      "
+ft_test "  2 *"
+ft_test "******"
+ft_test "2 2 #"
+ft_test "2 2.*"
+ft_test "2 2 * * *"
+ft_test "2 2 2 * *"
+ft_test "2 2 2 2 2 2 + + + + +ä"
+ft_test 3 2 *
+ft_test a s
 
 if [ $num -eq 0 ]
     then echo valgrind: ok
@@ -184,17 +42,11 @@ else
     echo valgrind: ko
 fi
 
-#echo "->out:"
-#cat out
-#echo "->expected:"
-#cat in
-#echo ----
-
 diff out in
 
 if [ $? -eq 0 ]
     then echo "tests:    ok"
-    rm out valgrind_test.txt
 else
     echo ko
 fi
+rm out valgrind_test.txt
