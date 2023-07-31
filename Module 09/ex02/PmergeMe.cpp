6dd126a28146308b40cd3aadad252cf13752c0dd
Niki42 <nik.7377@gmail.com>
2023-07-31 16:08:27 +0200

    started with merge sort

--- Module 09/ex02/PmergeMe.cpp
+++ Module 09/ex02/PmergeMe.cpp
@@ -7,6 +7,24 @@ PmergeMe::PmergeMe( vectorInt input ) : _vector(input)
 
 PmergeMe::~PmergeMe() {}
 
+//----------------------MergeSort----------------------//
+void PmergeMe::MergeSort()
+{
+    while(1)
+    {
+        
+    }
+}
+
+//--------------------InsertionSort--------------------//
+void PmergeMe::InsertionSort()
+{
+    for( vIntItr left = _vector.begin(); left != _vector.end(); left++ )
+        for( vIntItr right = left; right != _vector.end(); right++ )
+            if( left[0] > right[0])
+                moveNum( left, right );
+}
+
 void PmergeMe::moveNum( vIntItr &left, vIntItr &right )
 {
     vIntItr tmp = left;
@@ -14,14 +32,6 @@ void PmergeMe::moveNum( vIntItr &left, vIntItr &right )
     _vector.erase ( right );
     _vector.insert ( left, rightInttmp );
     left = tmp;
-}
-
-void PmergeMe::InsertionSort()
-{
-    for( vIntItr left = _vector.begin(); left != _vector.end(); left++ )
-        for( vIntItr right = left; right != _vector.end(); right++ )
-            if( left[0] > right[0])
-                moveNum( left, right );
 }
 
 void PmergeMe::printVector()
--- Module 09/ex02/PmergeMe.hpp
+++ Module 09/ex02/PmergeMe.hpp
@@ -5,20 +5,23 @@
 #include <iostream>
 
 class PmergeMe
-{
+{    
+
 private:
     typedef std::vector<int> vectorInt;
     typedef std::vector<int>::iterator vIntItr;
     vectorInt _vector;
 
     PmergeMe();
+    void MergeSort();
+    void InsertionSort();
     void moveNum( vIntItr &left, vIntItr &right );
-    
+
 public:
     PmergeMe( vectorInt input );
     ~PmergeMe();
+
     void printVector();
-    void InsertionSort();
 };
 
 #endif
