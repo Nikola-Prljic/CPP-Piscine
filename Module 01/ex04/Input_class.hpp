c5a7d99a29975776e696b5817682422983b6bf09
Niki42 <nik.7377@gmail.com>
2023-08-03 17:42:55 +0200

    sadsa

--- Module 01/ex04/Input_class.hpp
+++ Module 01/ex04/Input_class.hpp
@@ -6,7 +6,7 @@
 /*   By: nprljic <nprljic@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/08 18:32:49 by nprljic           #+#    #+#             */
-/*   Updated: 2023/08/03 17:10:17 by nprljic          ###   ########.fr       */
+/*   Updated: 2023/08/03 17:42:47 by nprljic          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +25,12 @@ class Input
         std::string     in_filename;
         std::fstream    out_file;
         std::string     out_filename;
+        Input( void );
+
         void    ft_open_infile( void );
         void    ft_open_outfile( void );
         void    copy_file( void );
         void    ft_replace( std::string& line );
-        Input( void );
     
     public:
 
