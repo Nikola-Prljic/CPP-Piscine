45435a19d51d930c406a7880273055d959a1de6e
Niki42 <nik.7377@gmail.com>
2023-08-03 16:08:06 +0200

    fixed makefile

--- Module 01/ex06/Harl.cpp
+++ Module 01/ex06/Harl.cpp
@@ -18,7 +18,7 @@ void Harl::complain( std::string level )
     switch (getInput(level))
     {
         case DEFAULT:
-            std::cout << "TRY: DEBUG INFO WARNING ERROR" << std::endl; break;
+            std::cout << "[ Probably complaining about insignificant problems ]" << std::endl; break;
         case DEBUG:
             debug();
         case INFO:
@@ -31,21 +31,21 @@ void Harl::complain( std::string level )
 }
 
 void Harl::debug(){
-    std::cout << "[DEBUG]" << std::endl;
+    std::cout << "[ DEBUG ]" << std::endl;
     std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-special-ketchup burger. I really do!" << std::endl;
 }
 
 void Harl::info(){
-    std::cout << "[INFO]" << std::endl;
+    std::cout << "[ INFO ]" << std::endl;
     std::cout << "I cannot believe adding extra bacon costs more money. You didn't put enough bacon in my burger! If you did, I wouldn’t be asking for more!" << std::endl;
 }
 
 void Harl::warning(){
-    std::cout << "[WARNING]" << std::endl;
+    std::cout << "[ WARNING ]" << std::endl;
     std::cout << "I think I deserve to have some extra bacon for free. I've been coming for years whereas you started working here since last month." << std::endl;
 }
 
 void Harl::error(){
-    std::cout << "[ERROR]" << std::endl;
+    std::cout << "[ ERROR ]" << std::endl;
     std::cout << "This is unacceptable! I want to speak to the manager now." << std::endl;
 }
