2e442ed651ca8ae34502da1e2a5c13ea7ef669d2
Niki42 <nik.7377@gmail.com>
2023-09-26 11:06:13 +0200

    fixed class make file wronggit add *git add *

--- Module 05/ex00/Bureaucrat.cpp
+++ Module 05/ex00/Bureaucrat.cpp
@@ -3,17 +3,14 @@
 Bureaucrat::Bureaucrat( const std::string name , int grade) : _name(name), _grade(grade)
 {
     if( _grade > 150 )
-        throw Bureaucrat::ToHigh();
-    if( _grade < 1 )
         throw Bureaucrat::ToLow();
+    if( _grade < 1 )
+        throw Bureaucrat::ToHigh();
     std::cout << "*Bureaucrat constructor called." << std::endl;
     return ;
 }
 
-Bureaucrat::Bureaucrat( Bureaucrat &src) : _name(src._name), _grade(src._grade)
-{
-    return ;
-}
+Bureaucrat::Bureaucrat( Bureaucrat &src) : _name(src._name), _grade(src._grade) { return ; }
 
 Bureaucrat::~Bureaucrat()
 {
@@ -28,31 +25,25 @@ Bureaucrat& Bureaucrat::operator=( Bureaucrat &rhs )
     return *this;
 }
 
-int Bureaucrat::getGrade( void ) const
-{
-    return _grade;
-}
+int Bureaucrat::getGrade( void ) const { return _grade; }
 
-std::string Bureaucrat::getName( void ) const
-{
-    return _name;
-}
+std::string Bureaucrat::getName( void ) const { return _name; }
 
 void Bureaucrat::increment( void )
 {
-    if((_grade + 1) > 150)
-         throw Bureaucrat::ToHigh();
+    if((_grade - 1) < 1)
+        throw Bureaucrat::ToLow();
     else
-        _grade++;
+        _grade--;
     return ;
 }
 
 void Bureaucrat::decrement( void )
 {
-    if((_grade - 1) < 1)
-         throw Bureaucrat::ToLow();
+    if((_grade + 1) > 150)
+        throw Bureaucrat::ToHigh();
     else
-        _grade--;
+        _grade++;
     return ;
 }
 
--- Module 05/ex00/main.cpp
+++ Module 05/ex00/main.cpp
@@ -2,19 +2,33 @@
 
 int main()
 {
-    try 
-    {
-        Bureaucrat nik("nik", -42);
+    try {
+        Bureaucrat nik("nik", 150);
         std::cout << nik << std::endl;
         nik.increment();
+        std::cout << nik << std::endl;
         nik.decrement();
+        std::cout << nik << std::endl;
     } 
-    catch (Bureaucrat::ToHigh &e) 
-    {
+    catch (Bureaucrat::ToHigh &e) {
         std::cout << e.what() << std::endl;
     }
-    catch (Bureaucrat::ToLow &e) 
-    {
+    catch (Bureaucrat::ToLow &e) {
+        std::cout << e.what() << std::endl;
+    }
+    std::cout << "------------------------" << std::endl;
+    try {
+        Bureaucrat nik("nik", 150);
+        Bureaucrat fio("fio", 144);
+        Bureaucrat copyOfFio(fio);
+        nik = fio;
+        std::cout << nik << std::endl;
+        std::cout << copyOfFio << std::endl;
+    } 
+    catch (Bureaucrat::ToHigh &e) {
+        std::cout << e.what() << std::endl;
+    }
+    catch (Bureaucrat::ToLow &e) {
         std::cout << e.what() << std::endl;
     }
     return 0;
